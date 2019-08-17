a021ee930d725decc663ee49964c0815fc3ea2ef
SmitiShakya <smitishk12@gmail.com>
2019-08-17 17:26:06 -0500

    Made drills

---
+++ functiondrills1/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//console.log('Hi, my name is Chris and I\'m 29 years old');
+
+function createGreeting() {
+  console.log('Hi, my name is Chris and I\'m 29 years old');
+  }
+  
+const greeting1 = createGreeting();
+console.log(greeting1);
